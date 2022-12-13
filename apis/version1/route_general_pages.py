39d0ccdb261c0ca15ef0895e9b069ea371f7b4ff
zharkynaskat <61348606+zharkynaskat@users.noreply.github.com>
2022-12-13 13:43:58 +0600

    initial commit

---
+++ apis/version1/route_general_pages.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+
+templates = Jinja2Templates(directory='templates')
+genera_pages_router = APIRouter()
+
+
+@genera_pages_router.get('/')
+async def home(request: Request):
+    return templates.TemplateResponse('general_pages/homepage.html',{'request': request})
